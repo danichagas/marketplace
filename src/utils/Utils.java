c82b7fb832207631f8bcf01b61162c61e4acf90d
Daniel Chagas <chagasdaniel788@gmail.com>
2024-04-26 16:50:49 -0300

    class utils finished

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -8,4 +8,8 @@ import java.util.Locale;
 public class Utils {
     static NumberFormat numberFormat =
             new DecimalFormat("R$ #,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
+
+    public static String doubleToString(String value) {
+        return numberFormat.format(value);
+    }
 }
