45814cc04efaf7a65b47eb9309f4e0e7bea5bf5d
Daniel Chagas <chagasdaniel788@gmail.com>
2024-04-26 17:34:31 -0300

    finished class Product

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,44 @@
 package model;
 
+import utils.Utils;
+
 public class Product {
+    private static int count = 1;
+
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.id = count;
+        this.name = name;
+        this.price = price;
+        Product.count += 1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return "Id: " + this.getId() +
+                "\nNome: " + this.getName() +
+                "\nPreço " + Utils.doubleToString(this.getPrice());
+    }
 }
